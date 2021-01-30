bec9fcbf0135a2fffb62650d89b1653273b5f602
Ran Crump <rancrump18@gmail.com>
2021-01-29 19:26:26 -0600

    + Pause Menu and Settings added plus start menu methods

---
+++ Assets/Scripts/Menu/MenuMethods.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMethods : MonoBehaviour
+{
+    public GameObject child;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            child.SetActive(!child.activeSelf);   
+        }
+    }
+    public void LoadScene(string sceneName) 
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Exit() 
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
---
+++ Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    // Sound
+    public AudioMixer audioMixer;
+    public Toggle toggleSound;
+    public bool volumeEnabled = true;
+    public float volume = 0, volumeAdder = 2;
+
+    // Graphics
+    public Resolution[] resolutions;
+    public TMP_Dropdown QualityDropDown;        
+    void Start()
+    {       
+        int Quality = PlayerPrefs.GetInt("_qualityIndex", 0);
+        volume = PlayerPrefs.GetFloat("_volume", 0);
+        volumeEnabled = (PlayerPrefs.GetFloat("_volumeEnabled", 1) == 1? true: false);
+        
+        SetVolume(volume);
+        ToggleVolume();
+
+        QualityDropDown.value = Quality;
+        resolutions = Screen.resolutions;
+        QualityDropDown.ClearOptions();
+
+        List<string> res = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++) {
+            res.Add(resolutions[i].width + " x " + resolutions[i].height);
+        }
+
+        QualityDropDown.AddOptions(res);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("_qualityIndex", qualityIndex);
+    }
+
+    public void SetVolume(float volume) 
+    {
+        if (volume < -20)
+            volume = -80;
+
+        PlayerPrefs.SetFloat("_volume", volume);
+        audioMixer.SetFloat("Volume", volume * volumeAdder);
+        this.volume = volume;
+    }
+
+    public void ToggleVolume()
+    {
+        volumeEnabled = !volumeEnabled;
+        audioMixer.SetFloat("Volume", volumeEnabled ? -80: volume);
+        PlayerPrefs.SetInt("_volumeEnabled", this.volumeEnabled? 1: 0);
+    }
+}
