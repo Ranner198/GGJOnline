35260a1d3d38949ef6c22d4b25a2ba8bab87debb
Lysle Shields <lysle@lesink.com>
2021-01-31 12:14:59 -0600

    Live phone now turns on the exit.

---
+++ Assets/LivePhone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivePhone : MonoBehaviour
+{
+    public void PhonedHome()
+    {
+        GameStateManager.Set("PhonedHome");
+    }
+}
