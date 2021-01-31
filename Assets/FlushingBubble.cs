c98276cb887d8a9af509a13eaa5dd4b9e41a5674
Lysle Shields <lysle@lesink.com>
2021-01-31 11:31:07 -0600

    Flashing bubble and main street sign.

---
+++ Assets/FlushingBubble.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlushingBubble : MonoBehaviour
+{
+    SpriteRenderer Renderer;
+
+    public float OnMin = 1.0f;
+    public float OnMax = 2.0f;
+    public float OffMin = 0.5f;
+    public float OffMax = 0.5f;
+    public string StateRequired;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+
+        if (GameStateManager.Is(StateRequired))
+        {
+            StartCoroutine(Flashing());
+        }
+        else
+        {
+            // We don't exist until we have that state
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator Flashing()
+    {
+        yield return 0;
+
+        while (true)
+        {
+            Renderer.enabled = true;
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+            Renderer.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.5f, 0.5f));
+        }
+    }
+}
