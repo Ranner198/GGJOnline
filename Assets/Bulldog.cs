410b031e0ae63bc64b032eb895ed8f6fab08a454
Lysle Shields <lysle@lesink.com>
2021-01-30 23:02:47 -0600

    Dog with dogdish. Working on steak placement.

--- Assets/Bulldog.cs
+++ Assets/Bulldog.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bulldog : MonoBehaviour
 {
+    public bool IsBlocking = true;
+
     public Transform RopeStart;
     public Transform RopeEnd;
     public GameObject Rope;
@@ -11,9 +13,10 @@ public class Bulldog : MonoBehaviour
     public Transform PoleEnd;
 
     // Notes
-    public float length;
-    public float calcLen;
-    public float scaleX;
+    private float length;
+    private float calcLen;
+    private float scaleX;
+    private Vector3 startPosition;
 
 
     // Start is called before the first frame update
@@ -23,11 +26,17 @@ public class Bulldog : MonoBehaviour
         Vector3 end = RopeEnd.position;
 
         length = (end - start).magnitude; // Corresponds to 1.0f in scale
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (IsBlocking)
+        {
+            transform.position = new Vector3(Movement.instance.gameObject.transform.position.x, startPosition.y + Movement.instance.gameObject.transform.position.x/40.0f, startPosition.z);
+        }
+
         Vector3 start = NeckStart.position;
         Vector3 end = PoleEnd.position;
         Rope.transform.position = start;
@@ -35,5 +44,6 @@ public class Bulldog : MonoBehaviour
         calcLen = (end - start).magnitude;
         scaleX = calcLen / length;
         Rope.transform.localScale = new Vector3(scaleX, 1.0f, 1.0f);
+
     }
 }
---
+++ Assets/Dogdish.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dogdish : MonoBehaviour
+{
+    public GameObject Steak;
+
+    public void SteakInBowl()
+    {
+        Steak.SetActive(true);
+    }
+}
