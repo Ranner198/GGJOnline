635787e6e14f7b9b082063a3cf377c18747cd5ac
Ran Crump <rancrump18@gmail.com>
2021-01-31 13:01:08 -0600

    + Phone Call

---
+++ Assets/Scripts/EnvokeAfterSeconds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnvokeAfterSeconds : MonoBehaviour
+{
+    public UnityEvent onAfterSeconds;
+    public float seconds;
+    void Start()
+    {
+        StartCoroutine(waitForSeconds());
+    }
+
+    IEnumerator waitForSeconds()
+    {
+        yield return new WaitForSeconds(seconds);
+
+        onAfterSeconds.Invoke();
+    }
+}
