e62629590a3bbb0ce085d77cf9af63c093c30061
Lysle Shields <lysle@lesink.com>
2021-01-31 14:20:45 -0600

    Get rid of persistant layer on a couple of scenes.

--- Assets/Scripts/Controllers/PersistManager.cs
+++ Assets/Scripts/Controllers/PersistManager.cs
@@ -5,14 +5,23 @@ public class PersistManager : MonoBehaviour {
 
 	public GameObject persistPrefab;    
 	public GameObject persistObject;
+	public bool DestroyPersist = false;
 
     void Awake () 
 	{
 		GameObject persist = GameObject.Find ("Persistent");
-		if (persist == null) 
+		if (DestroyPersist)
 		{
-            if (persistPrefab) persistObject = (GameObject)Instantiate(persistPrefab);
-			persistObject.name="Persistent";
-        }
+			if (persist)
+				Destroy(persist);
+		}
+		else
+		{
+			if (persist == null)
+			{
+				if (persistPrefab) persistObject = (GameObject)Instantiate(persistPrefab);
+				persistObject.name = "Persistent";
+			}
+		}
 	}
 }
